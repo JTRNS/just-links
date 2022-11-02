b868747453721b3ccfc2942bc67eae837bbcf712
Jeroen Trines <jeroen@trines.info>
2022-11-02 09:18:38 +0100

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <a href="/about">About us</a>
+    </div>
+  );
+}
